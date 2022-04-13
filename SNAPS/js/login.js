c89f1ef20ca8a65eac4fe6a11ec054ee52c468b5
Aleksander Balduino <aleksbalduino@gmail.com>
2022-04-13 16:56:11 -0300

    Att página login e add simulador cardio

--- SNAPS/js/login.js
+++ SNAPS/js/login.js
@@ -65,62 +65,160 @@ openSignUpButton.addEventListener("click", openSignUp, false);
 openSignInButton.addEventListener("click", openSignIn, false);
 
 /* Botão Show Hide Password */
-function ShowHidePasswordLogin() {
-  var x = document.getElementById("password-input-login");
-  if (x.type === "password") {
-    x.type = "text";
-    document.getElementById("show-hide-img-switch").src="../assets/images/show-password.png";
+function ShowHidePassword(e) {
+  var tipo = e.parentNode.querySelector("[id='password-input-sign-in']");
+  if (tipo.type == "password") {
+    tipo.type = "text";
   } else {
-    x.type = "password";
-    document.getElementById("show-hide-img-switch").src="../assets/images/hide-password.png";
+    tipo.type = "password";
+  }
+
+  tipo.type = tipo.type; //aplica o tipo que ficou no primeiro campo
+
+  if (e.classList.contains("uil-eye-slash")) { //se tem olho fechado
+    e.classList.remove("uil-eye-slash"); //remove classe olho fechado
+    e.classList.add("uil-eye"); //coloca classe olho aberto
+  } else { //senão
+    e.classList.remove("uil-eye"); //remove classe olho aberto
+    e.classList.add("uil-eye-slash"); //coloca classe olho fechado
   }
 }
 
-function ShowHidePasswordCadastro() {
-  var x = document.getElementById("password-input-cadastro");
-  if (x.type === "password") {
-    x.type = "text";
-    document.getElementById("show-hide-img-switch").src="../assets/images/show-password.png";
-  } else {
-    x.type = "password";
-    document.getElementById("show-hide-img-switch").src="../assets/images/hide-password.png";
-  }
-}
-
-function ShowHidePasswordConfirmCadastro() {
-  var x = document.getElementById("password-confirm-input-cadastro");
-  if (x.type === "password") {
-    x.type = "text";
-    document.getElementById("show-hide-img-switch").src="../assets/images/show-password.png";
-  } else {
-    x.type = "password";
-    document.getElementById("show-hide-img-switch").src="../assets/images/hide-password.png";
-  }
-}
-
+/* Recarregar página */
 function ReloadPage_Reset() {
-  document.getElementById('email-input-login').value='';
-  document.getElementById('email-input-cadastro').value='';
-  document.getElementById('f-option').checked='checked';
+  const nodeList = document.querySelectorAll(".txtOutros");
+  for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].value='';
+  }
 }
 
-function show1(){
-  document.getElementById('sign-up-form-professores').style.display ='block';
-  document.getElementById('sign-up-form-alunos').style.display ='none';
-  document.getElementById('sign-up-form-outros').style.display ='none';
-  document.getElementById('sign-up-form-professores').className = 'content-animation';
-}
+/* Select função */
+$(function(){
+    var closeSelectTimeout;
 
-function show2(){
-  document.getElementById('sign-up-form-professores').style.display = 'none';
-  document.getElementById('sign-up-form-alunos').style.display = 'block';
-  document.getElementById('sign-up-form-outros').style.display = 'none';
-  document.getElementById('sign-up-form-alunos').className = 'content-animation';
-}
+    function hideMaterialList(parent){
+        parent.css({
+            'overflow': 'hidden'
+        }).removeClass('isOpen');
+        clearTimeout(closeSelectTimeout);
+        closeSelectTimeout = setTimeout(function(){
+            parent.parent().css({
+                'z-index': 0  
+            });
+        }, 200);
+    }
+    $(document.body).on('mousedown', '.materialBtn, .select li', function(event){
+        if(parseFloat($(this).css('opacity')) > 0 && $(document).width() >= 1008){
+            var maxWidthHeight = Math.max($(this).width(), $(this).height());
+            if($(this).find("b.drop").length == 0 || $(this).find("b.drop").css('opacity') != 1) {
+                // .drop opacity is 1 when it's hidden...css animations
+                drop = $('<b class="drop" style="width:'+ maxWidthHeight +'px;height:'+ maxWidthHeight +'px;"></b>').prependTo(this);
+            }
+            else{
+                $(this).find("b.drop").each(function(){
+                    if($(this).css('opacity') == 1){
+                        drop = $(this).removeClass("animate");
+                        return;
+                    }
+                })
+            }
+            x = event.pageX - drop.width()/2 - $(this).offset().left;
+            y = event.pageY - drop.height()/2 - $(this).offset().top;
+            drop.css({
+                top: y,
+                left: x
+            }).addClass("animate");
+        }
+    });
+    $(document.body).on('dragstart', '.materialBtn, .select li', function(e){
+        e.preventDefault();
+    })
 
-function show3(){
-  document.getElementById('sign-up-form-professores').style.display = 'none';
-  document.getElementById('sign-up-form-alunos').style.display = 'none';
-  document.getElementById('sign-up-form-outros').style.display = 'block';
-  document.getElementById('sign-up-form-outros').className = 'content-animation';
-}
+    var selectTimeout;
+    $(document.body).on('click', '.select li', function() {
+        var parent = $(this).parent();
+        parent.children('li').removeAttr('data-selected');
+        $(this).attr('data-selected', 'true');
+        clearTimeout(selectTimeout);
+        if(parent.hasClass('isOpen')){
+            if(parent.parent().hasClass('required')){
+                if(parent.children('[data-selected]').attr('data-value')){
+                    parent.parents('.materialSelect').removeClass('error empty');
+                }
+                else{
+                    parent.parents('.materialSelect').addClass('error empty');
+                }
+            }
+            hideMaterialList($('.select'));
+            /*document.getElementById("txtOutros").focus();*/
+            const nodeList = document.querySelectorAll(".txtOutros");
+            for (let i = 0; i < nodeList.length; i++) {
+              nodeList[i].focus();
+            }
+        }
+        else{
+            var pos = Math.max(($('li[data-selected]', parent).index() - 2) * 48, 0);
+            parent.addClass('isOpen');
+            parent.parent().css('z-index', '999');
+            if($(document).width() >= 1008){
+                var i = 1;
+                selectTimeout = setInterval(function(){
+                    i++;
+                    parent.scrollTo(pos, 50);
+                    if(i == 2){
+                        parent.css('overflow', 'auto');
+                    }
+                    if(i >= 4){
+                        clearTimeout(selectTimeout);
+                    }
+                }, 100);
+            }
+            else{
+                parent.css('overflow', 'auto').scrollTo(pos, 0);
+            }
+        }
+    });
+
+    $('.materialInput input').on('change input verify', function(){
+        if($(this).attr('required') == 'true'){
+            if($(this).val().trim().length){
+                $(this).parent().removeClass('error empty');
+            }
+            else{
+                $(this).parent().addClass('error empty');
+                $(this).val('');
+            }
+        }
+        else{
+            if($(this).val().trim().length){
+                $(this).parent().removeClass('empty');
+            }
+            else{
+                $(this).parent().addClass('empty');
+            }
+        }
+    });
+
+    $(document.body).on('click', function(e) {
+        var clicked;
+        if($(e.target).hasClass('materialSelect')){
+            clicked = $(e.target).find('.select').first();
+        }
+        else if($(e.target).hasClass('select')){
+            clicked = $(e.target);
+        }
+        else if($(e.target).parent().hasClass('select')){
+            clicked = $(e.target).parent();
+        }
+
+        if($(e.target).hasClass('materialSelect') || $(e.target).hasClass('select') || $(e.target).parent().hasClass('select')){
+            hideMaterialList($('.select').not(clicked));
+        }
+        else{
+            if($('.select').hasClass('isOpen')){
+                hideMaterialList($('.select'));
+            }
+        }
+    });
+    hideMaterialList($('.select'));
+})
